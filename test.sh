f3c13f075e742520eacdc0a07bbc031709293683
George Bell <georgebell@George-Bells-MacBook-Pro.local>
2019-02-21 16:45:59 -0500

    Add Purpose, Vision and Values statements and cleanup skills

--- test.sh
+++ test.sh
@@ -1,6 +1,15 @@
 export ASK_DEFAULT_DEVICE_LOCALE=en-US
-ask simulate --text hello
+ask simulate --text "play the latest teaching from the meeting house"
+ask simulate --text "tell me about the meeting house"
+ask simulate --text "ask the meeting house for the vision statement"
+ask simulate --text "ask the meeting house for the purpose statement"
+ask simulate --text "ask the meeting house for their values"
+ask simulate --text "ask the meeting house about their values"
 
 export ASK_DEFAULT_DEVICE_LOCALE=en-CA
-ask simulate --text hello
+ask simulate --text "play the latest teaching from the meeting house"
+ask simulate --text "tell me about the meeting house"
+ask simulate --text "ask the meeting house for the vision statement"
+ask simulate --text "ask the meeting house for the mission statement"
+
 
